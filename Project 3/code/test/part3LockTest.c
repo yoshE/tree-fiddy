9740582d1b85f60e91db356c7d4b7f5809609a91
yoshE <yoshualo@usc.edu>
2015-07-17 04:55:13 -0700

    added more to the test.
    cant test wait though...

--- Project 3/code/test/part3LockTest.c
+++ Project 3/code/test/part3LockTest.c
@@ -1,15 +1,31 @@
 #include "syscall.h"
 
-int testLock;
-int testLock2;
-
 int main(){
 	int which;
+	int testLock;
+	int testCV;
+	
 	/*Creating and destroying a lock*/
-	testLock = CreateLock("test");
-	printf((int)"Created lock.\n", sizeof("Created lock.\n"), 0, 0);
+	testLock = CreateLock("testLock Create Destroy");
+	printf((int)"Created lock %d.\n", sizeof("Created lock %d.\n"), testLock, 0);
+	/* Creating, Acquiring, Releasing, and Destroying a lock */
+	testLock = CreateLock("testLock Acquire Release");
+	printf((int)"Created lock %d.\n", sizeof("Created lock %d.\n"), testLock, 0);
+	Acquire(testLock);
+	printf((int)"Acquired lock %d.\n", sizeof("Acquired lock %d.\n"), testLock, 0);
+	/* Creating, Signalling, Broadcasting, and Destroying a CV */
+	testCV = CreateCV("testCV Create Destroy");
+	printf((int)"Created CV %d.\n", sizeof("Created CV %d.\n"), testCV, 0);
+	Signal(testCV, testLock);
+	printf((int)"Signalled CV %d.\n", sizeof("Signalled CV %d.\n"), testCV, 0);
+	Broadcast(testCV, testLock);
+	ReleaseLock(testLock);
+	printf((int)"Released lock %d.\n", sizeof("Released lock $d.\n"), testLock, 0);
+	DestroyCV(testCV);
+	printf((int)"Destroyed CV.\n", sizeof("Destroyed CV.\n"), 0, 0);
 	DestroyLock(testLock);
-	printf((int)"Destroyed lock.\n", sizeof("Destroyed lock.\n"), 0, 0);
+	printf((int)"Destroyed lock.\n", sizeof("Destroyed lock.\n"), 0, 0);	
+	
 	Exit(0);
 /* changed file so i can push */
 }
