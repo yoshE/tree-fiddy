1ccd561a46b3e09f2cac7753a86bb3cdb09c3718
yoshE <yoshualo@usc.edu>
2015-07-25 17:13:43 -0700

    Airport Manager MVs

--- Project 4/code/test/AirportManager.c
+++ Project 4/code/test/AirportManager.c
@@ -6,17 +6,57 @@
 
 int main() {
 	int i;
-	for(i =0; i < simNumOfAirlines; i++){
-		simAirportManager.CIOTotalCount[i] = 0;
-		simAirportManager.CIOTotalWeight[i] = 0;
-		simAirportManager.LiaisonTotalCount[i] = 0;
-		simAirportManager.CargoHandlerTotalWeight[i] = 0;
-		simAirportManager.CargoHandlerTotalCount[i] = 0;
+	Initialize();
+	for(i = 0; i < MAX_AIRLINES; i++){
+		simAirportManager.CargoHandlerTotalWeight[i] = CreateMV("CargoHandlerTotalWeight ", 0, i);
+		simAirportManager.CargoHandlerTotalCount[i] = CreateMV("CargoHandlerTotalCount ", 0, i);
+		simAirportManager.CIOTotalCount[i] = CreateMV("CIOTotalCount ", 0, i);
+		simAirportManager.CIOTotalWeight[i] = CreateMV("CIOTotalWeight ", 0, i);
+		simAirportManager.LiaisonTotalCount[i] = CreateMV("LiaisonTotalCount ", 0, i);
+		simAirportManager.liaisonPassengerCount = CreateMV("liaisonPassengerCount ", 0, i);
+		simAirportManager.checkInPassengerCount = CreateMV("checkInPassengerCount ", 0, i);
+		simAirportManager.securityPassengerCount = CreateMV("securityPassengerCount ", 0, i);
 	}
-	simAirportManager.liaisonPassengerCount = 0;
-	simAirportManager.checkInPassengerCount = 0;
-	simAirportManager.securityPassengerCount = 0;
 	Manager_DoWork();
+}
+
+void EndOfDay(){
+	int i, j;
+	for(i = 0; i < simNumOfCargoHandlers; i++){
+		for(j = 0; j < simNumOfAirlines; j++){
+			SetMV(simAirportManager.CargoHandlerTotalWeight[j], simAirportManager.CargoHandlerTotalWeight[j] + GetMV(cargoHandlers[i].weight[j]));
+			SetMV(simAirportManager.CargoHandlerTotalCount[j], simAirportManager.CargoHandlerTotalCount[j] + GetMV(cargoHandlers[i].count[j]));
+		}
+	}
+	for(i = 0; i < simNumOfCIOs * simNumOfAirlines; i++){
+		for(j = 0; j < PASSENGER_COUNT*2; j++){
+			SetMV(simAirportManager.CIOTotalWeight[CheckIn[i].totalBags[j].airline], simAirportManager.CIOTotalWeight[CheckIn[i].totalBags[j].airline] + GetMV(CheckIn[i].totalBags[j].weight));
+		}
+		SetMV(simAirportManager.checkInPassengerCount, simAirportManager.checkInPassengerCount + GetMV(CheckIn[i].passengerCount));
+	}
+
+	for(i = 0; i < simNumOfLiaisons; i++){
+		for(j = 0; j < simNumOfAirlines; j++){
+			SetMV(simAirportManager.LiaisonTotalCount[j], simAirportManager.LiaisonTotalCount[j] + GetMV(liaisonOfficers[i].airlineBaggageCount[j]));
+		}
+		SetMV(simAirportManager.liaisonPassengerCount, simAirportManager.liaisonPassengerCount + GetMV(liaisonOfficers[i].passengerCount));
+	}
+	
+	for(i = 0; i < simNumOfScreeningOfficers; i++){
+		SetMV(simAirportManager.securityPassengerCount, simAirportManager.securityPassengerCount + GetMV(Security[i].PassedPassengers));
+	}
+	printf((int)"Passenger count reported by airport liaison = %d\n", sizeof("Passenger count reported by airport liaison = %d\n"), GetMV(simAirportManager.liaisonPassengerCount), -1);
+	printf((int)"Passenger count reported by airport liaison = %d\n", sizeof("Passenger count reported by airport liaison = %d\n"), GetMV(simAirportManager.checkInPassengerCount), -1);
+	printf((int)"Passenger count reported by security inspector = %d\n", sizeof("Passenger count reported by security inspector = %d\n"), GetMV(simAirportManager.securityPassengerCount), -1);
+	
+	for(i = 0; i < simNumOfAirlines; i++){
+		printf((int)"From setup: Baggage count of airline %d = %d\n", sizeof("From setup: Baggage count of airline %s = %d\n"), i, GetMV(totalBaggage[i]));
+		printf((int)"From airport liaison: Baggage count of airline %d = %d\n", sizeof("From airport liaison: Baggage count of airline %d = %d\n"), i, GetMV(simAirportManager.LiaisonTotalCount[i]));
+		printf((int)"From cargo handlers: Baggage count of airline %d = %d\n", sizeof("From cargo handlers: Baggage count of airline %d = %d\n"), i, GetMV(simAirportManager.CargoHandlerTotalCount[i]));
+		printf((int)"From setup: Baggage weight of airline %d = %d\n", sizeof("From setup: Baggage weight of airline %d = %d\n"), i, GetMV(totalWeight[i]));
+		printf((int)"From airline check-in staff: Baggage weight of airline %d = %d\n", sizeof("From airline check-in staff: Baggage weight of airline %d = %d\n"), i, GetMV(simAirportManager.CIOTotalWeight[i]));
+		printf((int)"From cargo handlers: Baggage weight of airline %d = %d\n", sizeof("From cargo handlers: Baggage weight of airline %d = %d\n"), i, GetMV(simAirportManager.CargoHandlerTotalWeight[i]));
+	}
 }
 
 void Manager_DoWork(){
@@ -24,12 +64,12 @@ void Manager_DoWork(){
 	while(true){
 		int i;
 		for (i = 0; i < PASSENGER_COUNT*2; i++){
-			if(conveyor[i].weight != 0){
+			if(GetMV(conveyor[i].weight) != 0){
 				Acquire(CargoHandlerLock);
 				breakCount = 0;
 				for(y = 0; y < simNumOfCargoHandlers; y++){
-				printf((int)"CH %d on break? %d\n", sizeof("CH %d on break? %d\n"), y, cargoHandlers[y].onBreak);
-					if(cargoHandlers[y].onBreak == 1){
+				printf((int)"CH %d on break? %d\n", sizeof("CH %d on break? %d\n"), y, GetMV(cargoHandlers[y].onBreak));
+					if(GetMV(cargoHandlers[y].onBreak) == 1){
 						breakCount++;
 					}	
 				}
@@ -47,10 +87,10 @@ void Manager_DoWork(){
 			printf("Boarding Lounge %d has passengers: %d\n", sizeof("Boarding Lounge %d has passengers: %d\n"), i, boardingLounges[i]);
 			printf("Total Baggage for %d has: %d\n", sizeof("Total Baggage for %d has: %d\n"), i, totalBaggage[i]);
 			printf("Aircraft Baggage for %d has: %d\n", sizeof("Aircraft Baggage for %d has: %d\n"), i, aircraftBaggageCount[i]);
-			if(boardingLounges[i] == totalPassengersOfAirline[i] && totalBaggage[i] == aircraftBaggageCount[i] && !alreadyBoarded[i]){
+			if(GetMV(boardingLounges[i]) == GetMV(totalPassengersOfAirline[i]) && GetMV(totalBaggage[i]) == GetMV(aircraftBaggageCount[i]) && !GetMV(alreadyBoarded[i])){
 				Acquire(gateLocks[i]);
 				Broadcast(gateLocksCV[i], gateLocks[i]);
-				alreadyBoarded[i] = true;
+				SetMV(alreadyBoarded[i], true);
 				Release(gateLocks[i]);
 				planeCount++;
 			}
